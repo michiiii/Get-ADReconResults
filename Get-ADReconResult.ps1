d7d9ba32d1c4ee26575e70f8b76b30bf6d3d75e0
Michael Ritter <info@michi-ritter.de>
2022-08-17 11:13:24 +0200

    Update Get-ADReconResult.ps1

--- Get-ADReconResult.ps1
+++ Get-ADReconResult.ps1
@@ -2159,11 +2159,11 @@ ForEach($sid in $SeMachineAccountPrivilege)
   $resource=(Convert-SidToName $sid.Substring(1))
   if($resource -eq "Authenticated Users")
   {
-    Write-Host "$($resource) has the SeMachineAccountPrivileges privileges" -ForegroundColor Black -BackgroundColor Red
+    Write-Host "The $($resource) group has the SeMachineAccountPrivileges privileges" -ForegroundColor Black -BackgroundColor Red
   }
   else
   {
-    Write-Host "$($resource) has the SeMachineAccountPrivileges privileges" -ForegroundColor Black -BackgroundColor Yellow
+    Write-Host "The $($resource) group has the SeMachineAccountPrivileges privileges" -ForegroundColor Black -BackgroundColor Yellow
   }
 }
 
